7d8053d4ae7d16db08891423eb857f93065924f7
draconian00 <draconian00@gmail.com>
2018-06-26 17:51:58 +0900

    init commit & test.js

---
+++ README.md
@@ -0,0 +1,6 @@
+# Test for face-recognition.js
+
+### JS API
+- https://github.com/justadudewhohacks/face-recognition.js#with-opencv4nodejs
+
+javascript 얼굴인식 테스트
---
+++ test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const fr = require('face-recognition');
+const detector = fr.FaceDetector();
+
+const freeze = () => {
+  fr.hitEnterToContinue();
+}
+
+const origin_img = fr.loadImage('./images/face1.jpg');
+
+const win = new fr.ImageWindow();
+// display image
+win.setImage(origin_img);
+
+const faceRectangles = detector.locateFaces(origin_img);
+const faceImages = detector.detectFaces(origin_img);
+
+win.addOverlay(faceRectangles[0].rect);
+const win2 = new fr.ImageWindow();
+win2.setImage(faceImages[0]);
+
+freeze();
